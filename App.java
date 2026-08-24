c4151cdcf0af4c9ae8600e836a2b5f6ae1d858e5
desilva <nsdesilva5@gmail.com>
2026-08-24 21:33:52 +0530

    modify: UI

--- App.java
+++ App.java
@@ -9,5 +9,16 @@ class App{
 			frame.setLayout(null);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
+
+			HomeUI homeUI = new HomeUI();
+			frame.add(homeUI);
+
+			JLabel label = new JLabel("Courier Management System");
+			label.setBounds(400, 50, 200, 50);
+			label.addNotify();
+			label.setHorizontalAlignment(SwingConstants.CENTER);
+			label.setForeground(Color.blue);
+			label.setFont(new Font("Arial", Font.BOLD, 24));
+			frame.add(label);
 		}
 }
