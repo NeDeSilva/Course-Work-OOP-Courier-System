b475f8430d8909841be9b1165734410a593ca4b4
Sasiru Nethmin <sasirunethmin1999@gmail.com>
2026-08-26 22:50:41 +0530

    Update Customer.java

--- BusinessLogic/Customer.java
+++ BusinessLogic/Customer.java
@@ -1,3 +1,28 @@
-class Customer {
-	
+package BusinessLogic;
+
+public class Customer extends Person {
+    private String address;
+    private String contactNumber;
+
+    public Customer(String id, String name, String address, String contactNumber) {
+        super(id, name);
+        this.address = address;
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 }
