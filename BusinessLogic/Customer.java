cba230c0d145e3cc146acd77479e39a9175c2ff9
Sasiru Nethmin <sasirunethmin1999@gmail.com>
2026-08-29 22:37:16 +0530

    Update Customer.java

--- BusinessLogic/Customer.java
+++ BusinessLogic/Customer.java
@@ -1,7 +1,1 @@
-package BusinessLogic;
 
-class Customer extends Person{
-	public Customer(String govID, String name, int age, String address, String phoneNumber, String emailAddress, String userName, String password) {
-		super(govID, name, age, address, phoneNumber, emailAddress, userName, password);
-	}
-}
